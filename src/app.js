204997ac355e227c520d5ad0391a514a625303e7
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-03-11 12:11:21 +0900

    쓸데없이 왜 옛날버전으로 만든거야

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const PORT = 3000;
 app.use(express.json());
 
 
-app.use("/api",postRouter);
+app.use("/",postRouter);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -4,7 +4,7 @@ import { prisma } from "../utils/prisma/index.js";
 const router = express.Router();
 
 // 게시글 작성
-router.post("/posts", async (req, res) => {
+const createNewPost  = async (req, res) => {
   const { title, content } = req.body;
 
   const newpost = await prisma.Posts.create({
@@ -17,10 +17,10 @@ router.post("/posts", async (req, res) => {
   return res
     .status(201)
     .json({ id: newpost.id, title: newpost.title, content: newpost.content });
-});
+};
 
 // 게시글 조회
-router.get("/posts", async (req, res) => {
+const getAllPosts = async (req, res) => {
   const categorylist = await prisma.Posts.findMany({
     select: {
       id: true,
@@ -30,10 +30,10 @@ router.get("/posts", async (req, res) => {
   });
 
   return res.status(200).json(categorylist);
-});
+};
 
 // 카테고리 정보 변경 API
-router.put("/posts/:id", async (req, res) => {
+const updatePost  = async (req, res) => {
   const { title, content } = req.body;
   const { id } = req.params;
   const updatedPost = await prisma.Posts.update({
@@ -46,10 +46,10 @@ router.put("/posts/:id", async (req, res) => {
     },
   });
   return res.json({ id: updatedPost.id, title: updatedPost.title, content: updatedPost.content });
-});
+};
 
 // 카테고리 삭제 API
-router.delete("/posts/:id", async (req, res) => {
+const deletePostById  = async (req, res) => {
   const { id } = req.params;
 
   await prisma.Posts.delete({
@@ -58,6 +58,12 @@ router.delete("/posts/:id", async (req, res) => {
     },
   });
   return res.status(200).json({ message: "석세스" });
-});
+};
 
+
+
+router.post('/api/posts', createNewPost);
+router.get('/api/posts', getAllPosts);
+router.put('/api/posts/:postId', updatePost);
+router.delete('/api/posts/:postId', deletePostById);
 export default router;
